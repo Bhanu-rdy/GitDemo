98b5214a6f1afb139676ccabf479a6b8059ed10b
Bhanu-rdy <bhanuprakashreddyvangala@gmail.com>
2024-08-10 19:48:28 +0530

    modified after cloning

--- Launch.java
+++ Launch.java
@@ -3,6 +3,16 @@ public class Launch
  public static void main(String[] args)
  {
    System.out.println("git demonstration");
+  
+   public void fun()
+  {
+    int a = 10;
+    int b =3;
+    int sum = a+b;
+    System.out.println(sum);
+  }
+ 
  
  }
+ 
 }
